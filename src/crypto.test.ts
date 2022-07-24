14914279aadd55b90c538b3921a08e4197b120db
Okamato <106880392+Okamato@users.noreply.github.com>
2022-07-24 20:37:55 +0000

    README.MD

--- src/crypto.test.ts
+++ src/crypto.test.ts
@@ -1,2 +1,20 @@
 
 import makeCryptoWith, { Crypto } from './crypto';
+
+describe('crypto', () => {
+  let crypto: Crypto;
+
+  beforeEach(() => {
+    const opts = {
+      encryptionKey: 'no wifi on this plane :(',
+    };
+    crypto = makeCryptoWith(opts);
+  });
+
+  it('throws if CryptoOptions does not include encryptionKey', () => {
+    expect(() => {
+      makeCryptoWith({} as any);
+    }).toThrow();
+  });
+
+  describe('encrypt()', () => {
